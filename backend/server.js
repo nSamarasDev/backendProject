86fe648b95183b6891c97a33f548d31043eb0ac5
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-18 14:47:33 -0400

    status update

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to Shaheen admistration API' });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
