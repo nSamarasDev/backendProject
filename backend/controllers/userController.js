810d2a61841e5b58e11d0d787818fdc4b183127c
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-19 15:51:45 -0400

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,26 @@
-const registerUser = (req, res) => {
-  res.send('Register Route');
-};
+const asyncHandler = require('express-async-handler')
 
-const loginUser = (req, res) => {
+// @dec    Register a new user
+// @route  /api/users
+//@access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    // Validation. 400 response is client side error
+    res.status(400);
+    throw new Error('Please include all fields');
+  }
+
+  res.send('Register Route');
+})
+
+// @dec    Login a new user
+// @route  /api/users/login
+//@access  Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send('login Route');
-};
+})
 
 module.exports = {
   registerUser,
