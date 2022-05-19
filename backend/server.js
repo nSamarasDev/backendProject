96ac61eabfd06b2b524705bd20bb2c9847547b03
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-19 15:53:27 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const morgan = require('morgan');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to Shaheen admistration API' });
@@ -10,5 +19,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
