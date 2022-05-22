f4fd2ed22bc705bed1b3c9c86dc1e0a4207ecb59
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-22 10:56:55 -0400

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
 const morgan = require('morgan');
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
+
+// Connect to DB
+connectDB();
 
 const app = express();
 
