45d713aea47b420703896cdf6d78a4d761c0f8ae
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-24 16:26:30 -0400

    auth middleware added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -73,14 +73,17 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route  /api/users/me
 //@access  Private
 const getMe = asyncHandler(async (req, res) => {
-  res.send('me');
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
 });
 
 // Generate Token
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  });
+  return jwt.sign({ id }, process.env.JWT_SECRET, {});
 };
 
 module.exports = {
